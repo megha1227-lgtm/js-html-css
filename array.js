75c1a2f9bcb08520f87d526d465dd1b8c6da983c
megha sharma <meghasharma123tt@gmail.com>
2025-12-19 05:24:23 +0000

    push array.js and array2.js

---
+++ array.js
@@ -0,0 +1,39 @@
+//array is resizable in js mean we can add new values
+//zero base indexing 
+
+const myArr =[ 0,1,2,3,4,5]
+const myHero = ["ironmen", "batmen", ]
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[1]);
+
+//array methods 
+
+// myArr.push(6, 7, 8) //adding array
+// myArr.pop() //remove last value in array 
+// myArr.unshift(8)
+myArr.shift()
+
+console.log(myArr.includes(9));// answering the question 
+console.log(myArr.indexOf(9));
+console.log(myArr.indexOf(3));
+
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr);
+
+
+
+//slice , splice 
+//slice doesn't change array but when it comes to splice its reflect the opposite in splice is array
+console.log("A", myArr);
+
+const myArr1= myArr.slice(1,3);
+console.log(myArr1);
+console.log("b", myArr);
+
+
+const myA2 = myArr.splice(1,3);
+console.log("c", myArr);
+console.log( myA2);
---
+++ array2.js
@@ -0,0 +1,16 @@
+const marvel_heros = ["thor","ironman", "spider"]
+const dc_heros = ["superman", "flash", "batman"]
+
+
+marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allmarvel_heros =marvel_heros.concat(dc_heros)
+
+// console.log(allmarvel_heros);
+
+
+const all_new_heros= [...marvel_heros,...dc_heros]
+console.log(all_new_heros);
