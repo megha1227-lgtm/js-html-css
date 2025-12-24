844b2f615569ebf0b374a1bfb57b733c817a843b
megha sharma <meghasharma123tt@gmail.com>
2025-12-24 09:24:41 +0530

    Add comment on variable declaration preference
    Added a comment explaining the preference for using 'let' or 'const' over 'var'.

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ accountCity = 'delhi'
 console.log(accountId);
 
 console.table([accountEmail, accountId , accountPassword , accountCity, accountState])
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
